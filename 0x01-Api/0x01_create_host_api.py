9e9a5b1c86fc573a60e1de15c19def38f09b942f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-09-10 01:27:07 +0100

    feat: :sparkles: code refactor to include header and also changing the end point

--- 0x01-Api/0x01_create_host_api.py
+++ 0x01-Api/0x01_create_host_api.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
 """Create host api."""
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import datetime
-import pytz
 
 app = Flask(__name__)
 
 
-@app.route('/endpoint', methods=['GET'])
+@app.route("/api", methods=["GET"])
 def endpoint():
-    slack_name = request.args.get('slack_name')
-    track = request.args.get('track')
-
     # Get current day of the week
     day_of_week = datetime.datetime.now().strftime("%A")
 
     # Get current UTC time with validation of +/-2 hours
-    utc_now = datetime.datetime.now(pytz.utc)
-    utc_offset = utc_now.utcoffset().total_seconds() / 3600
-
-    if utc_offset < -2 or utc_offset > 2:
-        return jsonify({"error": "Invalid UTC offset"}), 400
+    utc_now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
     # Get GitHub URLs
     file_url = "https://github.com/Yusuf-R/IGNIX/blob/main/0x01-Api/0x01_create_host_api.py"
-    source_code_url = 'https://github.com/Yusuf-R/IGNIX'
+    source_code_url = "https://github.com/Yusuf-R/IGNIX"
 
-    response = {
-        "slack_name": slack_name,
+    data = {
+        "slack_name": "Abdulwasiu Yusuf",
         "day_of_week": day_of_week,
-        "utc_time": utc_now.strftime("%Y-%m-%d %H:%M:%S"),
-        "track": track,
+        "utc_time": utc_now,
+        "track": "backend",
         "file_url": file_url,
-        "source_code_url": source_code_url
+        "source_code_url": source_code_url,
+        "status_code": 200,
     }
 
-    return jsonify(response), 200
+    response = jsonify(data)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 
-if __name__ == '__main__':
-    app.run(port=5500, debug=True)
+if __name__ == "__main__":
+    app.run()
