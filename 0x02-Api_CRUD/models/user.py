10a43735488c3b3f1574cca495cb25e005505bd2
Yusuf-R <y.abdulwasiu@gmail.com>
2023-09-12 23:12:43 +0100

    feat: :sparkles: the user class for object instance

---
+++ 0x02-Api_CRUD/models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This the base template for all model object instances."""
+
+from models.parent import Parent, Base
+from sqlalchemy import Column, String
+
+
+class User(Parent, Base):
+    """User class"""
+    __tablename__ = "users"
+    user_name = Column(String(32), unique=True, nullable=False)
